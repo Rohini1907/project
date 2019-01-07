864c20d52c624393250940e3b6ddd9075f9c3639
Rohini1907 <46449897+Rohini1907@users.noreply.github.com>
2019-01-07 20:10:35 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if($method == "POST"){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+	$text = $json->result->parameters->text;
+
+	switch ($text) {
+		case 'Assam':
+			$speech = "32 Million";
+			break;
+
+		case 'Delhi':
+			$speech = "27 Million";
+			break;
+
+		case 'Goa':
+			$speech = "2 Million";
+			break;
+
+		case 'Karnataka':
+			$speech = "62 Million";
+			break;
+
+		case 'Rajasthan':
+			$speech = "68 Million";
+			break;
+		
+		default:
+		    $speech = "Sorry, I don't get that.please ask me something else."
+            break;
+	}
+
+	$response = new \stdClass();
+	$response->speech = "";
+	$response->displayText = "";
+	$response->source = "webhook";
+	echo json_encode($response);
+
+}else
+{
+	echo "Method not allowed";
+}
+
+
+
+?>
